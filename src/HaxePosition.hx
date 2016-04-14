188a1c42cd48d2dbe138a683bcfcb2c3d95e3f9b
Dan Korostelev <nadako@gmail.com>
2016-04-14 18:42:27 +0300

    factor out HaxePosition handling

---
+++ src/HaxePosition.hx
@@ -0,0 +1,54 @@
+@:forward
+abstract HaxePosition(HaxePositionData) from HaxePositionData to HaxePositionData {
+    static var positionRe = ~/^(.+):(\d+): (?:lines (\d+)-(\d+)|character(?:s (\d+)-| )(\d+))$/;
+
+    public static function parse(pos:String):Null<HaxePosition> {
+        return if (positionRe.match(pos))
+            {
+                file: positionRe.matched(1),
+                line: Std.parseInt(positionRe.matched(2)),
+                startLine: Std.parseInt(positionRe.matched(3)),
+                endLine: Std.parseInt(positionRe.matched(4)),
+                startByte: Std.parseInt(positionRe.matched(5)),
+                endByte: Std.parseInt(positionRe.matched(6)),
+            }
+        else
+            null;
+    }
+
+    public function toRange():vscode.BasicTypes.Range {
+        var pos = this;
+        var startLine = if (pos.startLine != null) pos.startLine - 1 else pos.line - 1;
+        var endLine = if (pos.endLine != null) pos.endLine - 1 else pos.line - 1;
+        var startChar = 0;
+        var endChar = 0;
+        
+        // if we have byte offsets within line, we need to convert them to character offsets
+        // for that we have to read the file :-/
+        var lines = null;
+        inline function getLineChar(line:Int, byteOffset:Int):Int {
+            if (lines == null) lines = sys.io.File.getContent(pos.file).split("\n");
+            var lineContent = new js.node.Buffer(lines[line], "utf-8");
+            var lineTextSlice = lineContent.toString("utf-8", 0, byteOffset);
+            return lineTextSlice.length;
+        }
+        if (pos.startByte != null && pos.startByte != 0)
+            startChar = getLineChar(startLine, pos.startByte);
+        if (pos.endByte != null && pos.endByte != 0)
+            endChar = getLineChar(endLine, pos.endByte);
+
+        return {
+            start: {line: startLine, character: startChar},
+            end: {line: endLine, character: endChar},
+        };
+    }
+}
+
+typedef HaxePositionData = {
+    file:String,
+    line:Int, // 1-based
+    startLine:Null<Int>, // 1-based
+    endLine:Null<Int>, // 1-based
+    startByte:Null<Int>, // 0-based byte offset
+    endByte:Null<Int>, // 0-based byte offset
+}
--- src/Main.hx
+++ src/Main.hx
@@ -14,32 +14,7 @@ import sys.FileSystem;
 using StringTools;
 import Uri.*;
 
-typedef HaxePosition = {
-    file:String,
-    line:Int, // 1-based
-    startLine:Null<Int>, // 1-based
-    endLine:Null<Int>, // 1-based
-    startByte:Null<Int>, // 0-based byte offset
-    endByte:Null<Int>, // 0-based byte offset
-}
-
 class Main {
-    static var positionRe = ~/^(.+):(\d+): (?:lines (\d+)-(\d+)|character(?:s (\d+)-| )(\d+))$/;
-
-    static function parsePosition(pos:String):HaxePosition {
-        return if (positionRe.match(pos))
-            {
-                file: positionRe.matched(1),
-                line: Std.parseInt(positionRe.matched(2)),
-                startLine: Std.parseInt(positionRe.matched(3)),
-                endLine: Std.parseInt(positionRe.matched(4)),
-                startByte: Std.parseInt(positionRe.matched(5)),
-                endByte: Std.parseInt(positionRe.matched(6)),
-            }
-        else
-            null;
-    }
-
     static function main() {
         var reader = new MessageReader(process.stdin);
         var writer = new MessageWriter(process.stdout);
@@ -167,15 +142,15 @@ class Main {
 
                     var results = [];
                     for (p in positions) {
-                        var pos = parsePosition(p);
+                        var pos = HaxePosition.parse(p);
                         if (pos == null) {
                             trace("Got invalid position: " + p);
                             continue;
                         }
-                        trace(pos, haxePositionToRange(pos));
+                        trace(pos, pos.toRange());
                         results.push({
                             uri: fsPathToUri(pos.file),
-                            range: haxePositionToRange(pos)
+                            range: pos.toRange(),
                         });
                     }
 
@@ -193,31 +168,6 @@ class Main {
         reader.listen(proto.handleMessage);
     }
 
-    static function haxePositionToRange(pos:HaxePosition):Range {
-        var startLine = if (pos.startLine != null) pos.startLine - 1 else pos.line - 1;
-        var endLine = if (pos.endLine != null) pos.endLine - 1 else pos.line - 1;
-        var startChar = 0;
-        var endChar = 0;
-        
-        // if we have byte offsets within line, we need to convert them to character offsets
-        // for that we have to read the file :-/
-        var lines = null;
-        inline function getLineChar(line:Int, byteOffset:Int):Int {
-            if (lines == null) lines = sys.io.File.getContent(pos.file).split("\n");
-            var lineContent = new Buffer(lines[line], "utf-8");
-            var lineTextSlice = lineContent.toString("utf-8", 0, byteOffset);
-            return lineTextSlice.length;
-        }
-        if (pos.startByte != null && pos.startByte != 0)
-            startChar = getLineChar(startLine, pos.startByte);
-        if (pos.endByte != null && pos.endByte != 0)
-            endChar = getLineChar(endLine, pos.endByte);
-
-        return {
-            start: {line: startLine, character: startChar},
-            end: {line: endLine, character: endChar},
-        };
-    }
 
     static function parseFieldCompletion(x:Xml):Array<CompletionItem> {
         var result = [];
