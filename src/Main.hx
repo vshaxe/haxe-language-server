f079503c05e6e0beb8e0da031a4099862d76bf7b
Dan Korostelev <nadako@gmail.com>
2016-04-18 15:21:14 +0300

    add document symbols

--- src/Main.hx
+++ src/Main.hx
@@ -40,6 +40,7 @@ class Main {
                     definitionProvider: true,
                     hoverProvider: true,
                     referencesProvider: true,
+                    documentSymbolProvider: true,
                 }
             });
         };
@@ -200,6 +201,48 @@ class Main {
                     reject(0, "no info");
                 else
                     resolve(results);
+            });
+        }
+
+        proto.onDocumentSymbols = function(params, cancelToken, resolve, reject) {
+            var doc = context.getDocument(params.textDocument.uri);
+            var filePath = uriToFsPath(params.textDocument.uri);
+            var args = [
+                "--display", '$filePath@0@document-symbols'
+            ];
+            var stdin = if (doc.saved) null else doc.content;
+            context.callDisplay(args, stdin, cancelToken, function(data) {
+                if (cancelToken.canceled)
+                    return;
+
+                var data:Array<{name:String, kind:Int, location:String, ?containerName:String}> =
+                    try haxe.Json.parse(data) catch (e:Dynamic) {
+                        trace("INVALID document-symbols: " + e);
+                        trace("First 4096 symbols:\n" + data.substr(0, 4096));
+                        return reject(ErrorCodes.InternalError, "Error parsing document symbol response: " + e);
+                    }
+
+                var result = new Array<SymbolInformation>();
+                for (v in data) {
+                    var pos = HaxePosition.parse(v.location);
+                    if (pos == null) {
+                        proto.sendShowMessage({type: Error, message: "Couldn't parse position for " + haxe.Json.stringify(v)});
+                        continue;
+                    }
+                    var item:SymbolInformation = {
+                        name: v.name,
+                        kind: cast v.kind,
+                        location: {
+                            uri: params.textDocument.uri, // should be the same i guess
+                            range: pos.toRange()
+                        }
+                    };
+                    if (v.containerName != null)
+                        item.containerName = v.containerName;
+                    result.push(item);
+                }
+
+                resolve(result);
             });
         }
 
