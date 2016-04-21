8a2307c5c34dfe2591d6920232fcf78e36aa3233
Dan Korostelev <nadako@gmail.com>
2016-04-21 20:07:33 +0300

    move getProperFileNameCase to HaxePosition as it's actually should only be used with normalized paths from haxe positions

--- src/HaxePosition.hx
+++ src/HaxePosition.hx
@@ -1,11 +1,13 @@
 @:forward
 abstract HaxePosition(HaxePositionData) from HaxePositionData to HaxePositionData {
     static var positionRe = ~/^(.+):(\d+): (?:lines (\d+)-(\d+)|character(?:s (\d+)-| )(\d+))$/;
+    static var properFileNameCaseCache:Map<String,String>;
+    static var isWindows = (Sys.systemName() == "Windows");
 
     public static function parse(pos:String):Null<HaxePosition> {
         return if (positionRe.match(pos))
             {
-                file: FsUtils.getProperFileNameCase(positionRe.matched(1)),
+                file: getProperFileNameCase(positionRe.matched(1)),
                 line: Std.parseInt(positionRe.matched(2)),
                 startLine: Std.parseInt(positionRe.matched(3)),
                 endLine: Std.parseInt(positionRe.matched(4)),
@@ -14,6 +16,34 @@ abstract HaxePosition(HaxePositionData) from HaxePositionData to HaxePositionDat
             }
         else
             null;
+    }
+
+    static function getProperFileNameCase(normalizedPath:String):String {
+        if (!isWindows) return normalizedPath;
+        if (properFileNameCaseCache == null) {
+            properFileNameCaseCache = new Map();
+        } else {
+            var cached = properFileNameCaseCache[normalizedPath];
+            if (cached != null)
+                return cached;
+        }
+        var result = normalizedPath;
+        var parts = normalizedPath.split("\\");
+        if (parts.length > 1) {
+            var acc = parts[0];
+            for (i in 1...parts.length) {
+                var part = parts[i];
+                for (realFile in sys.FileSystem.readDirectory(acc)) {
+                    if (realFile.toLowerCase() == part) {
+                        part = realFile;
+                        break;
+                    }
+                }
+                acc = acc + "/" + part;
+            }
+            result = acc;
+        }
+        return properFileNameCaseCache[normalizedPath] = result;
     }
 }
 
