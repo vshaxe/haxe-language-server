3ee1d564d1d6862d66855674c0269edde8e1294c
Dan Korostelev <nadako@gmail.com>
2016-05-19 16:51:52 +0300

    extract json-rpc stuff into a submodule (closes #11)

--- src/vscodeProtocol/Protocol.hx
+++
@@ -1,8 +0,0 @@
-package vscodeProtocol;
-
-@:build(jsonrpc.ProtocolMacro.build("vscodeProtocol.Types.MethodNames"))
-class Protocol extends jsonrpc.Protocol {
-    override function logError(message:String) {
-        sendLogMessage({type: Warning, message: message});
-    }
-}
