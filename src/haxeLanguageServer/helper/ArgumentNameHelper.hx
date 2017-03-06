vshaxe/haxe-language-server

commit 942470dc08509e6b0641093e09e26ff5dc4364db

Insert argument names in signature completion
see HaxeFoundation/haxe#6064

src/haxeLanguageServer/helper/ArgumentNameHelper.hx
@@ -1,5 +1,7 @@
 package haxeLanguageServer.helper;
 
+import String.fromCharCode;
+
 class ArgumentNameHelper {
     public static function guessArgumentNames(types:Array<String>):Array<String> {
         return avoidDuplicates([for (type in types) guessArgumentName(type)]);
@@ -34,5 +36,37 @@ class ArgumentNameHelper {
             if (i > 0) name = name + i;
             name;
         }];
+    }
+
+    /**
+        Adds argument names to types from signature completion.
+
+        @param  type   a type like `(:Int, :Int):Void` or `:Int`
+        @see https://github.com/HaxeFoundation/haxe/issues/6064
+    **/
+    public static function addNamesToSignatureType(type:String, index:Int = 0):String {
+        inline function getUniqueLetter(index:Int) {
+            var letters = 26;
+            var alphabets = Std.int(index / letters) + 1;
+            var lowerAsciiA = 0x61;
+            return [for (i in 0...alphabets) fromCharCode(lowerAsciiA + (index % letters))].join("");
+        }
+
+        if (type.startsWith(":"))
+             return getUniqueLetter(index) + type;
+        else if (type.startsWith("(")) {
+            var segmentsRe = ~/\((.*?)\)\s*:\s*(\w+)/;
+            if (!segmentsRe.match(type))
+                return type;
+            var args = segmentsRe.matched(1);
+            var returnType = segmentsRe.matched(2);
+            var fixedArgs = [for (arg in ~/\s*,\s*/g.split(args)) {
+                var fixedArg = addNamesToSignatureType(arg, index);
+                index++;
+                fixedArg;
+            }];
+            return '(${fixedArgs.join(", ")}):$returnType';
+        }
+        return type;
     }
 }

test/haxeLanguageServer/helper/ArgumentNameHelperTest.hx
@@ -26,4 +26,24 @@ class ArgumentNameHelperTest extends TestCaseBase {
         assert("c", "C<Int,String>");
         assert("unknown", "<Int,String>");
     }
+
+    function testAddNamesToSignatureType() {
+        function assert(expected, original, ?posInfos)
+            assertEquals(expected, ArgumentNameHelper.addNamesToSignatureType(original), posInfos);
+
+        assert("a:Int", ":Int");
+        assert("a:Int", "a:Int");
+
+        assert("(a:Int, b:Int):Void", "(:Int, :Int):Void");
+        assert("(a:Int, b:Int):Void", "(a:Int, b:Int):Void");
+        assert("(a:Int, b:Int, c:Int):Void", "(:Int, :Int,:Int) : Void");
+
+        assert("(", "(");
+
+        // hopefully this is never needed...
+        var letterOverflow = '(${[for (i in 0...30) ":Int"].join(", ")}):Void';
+        var fixedSignature = ArgumentNameHelper.addNamesToSignatureType(letterOverflow);
+        assertEquals(-1, fixedSignature.indexOf("{:")); // { comes after z in ascii
+        assertEquals(2, fixedSignature.split(" b:").length); // arg names must be unique
+    }
 }

src/haxeLanguageServer/features/SignatureHelpFeature.hx
@@ -1,5 +1,6 @@
 package haxeLanguageServer.features;
 
+import haxeLanguageServer.helper.ArgumentNameHelper.addNamesToSignatureType;
 import haxeLanguageServer.helper.DocHelper;
 import jsonrpc.CancellationToken;
 import jsonrpc.ResponseError;
@@ -29,8 +30,13 @@ class SignatureHelpFeature {
                     resolve(null);
                 case DResult(data):
                     var help:SignatureHelp = haxe.Json.parse(data);
-                    for (signature in help.signatures)
+                    for (signature in help.signatures) {
                         signature.documentation = DocHelper.extractText(signature.documentation);
+                        var parameters = signature.parameters;
+                        for (i in 0...signature.parameters.length)
+                            parameters[i].label = addNamesToSignatureType(parameters[i].label, i);
+                        signature.label = addNamesToSignatureType(signature.label);
+                    }
                     resolve(help);
 
                     if (currentSignature != null) {

commit 6ec52a43215259430195fc9271643b67c8a2b0ee

Signature help: fix return type paths ending after first dot
This was a regression from 942470d.

src/haxeLanguageServer/helper/ArgumentNameHelper.hx
@@ -56,7 +56,7 @@ class ArgumentNameHelper {
         if (type.startsWith(":"))
              return getUniqueLetter(index) + type;
         else if (type.startsWith("(")) {
-            var segmentsRe = ~/\((.*?)\)\s*:\s*(\w+)/;
+            var segmentsRe = ~/\((.*?)\)\s*:\s*([\w.]+)/;
             if (!segmentsRe.match(type))
                 return type;
             var args = segmentsRe.matched(1);

test/haxeLanguageServer/helper/ArgumentNameHelperTest.hx
@@ -53,6 +53,8 @@ class ArgumentNameHelperTest extends TestCaseBase {
         assert("(a:Int, b:Int, c:Int):Void", "(:Int, :Int,:Int) : Void");
 
         assertUnchanged("(");
+        assertUnchanged("():haxe.ds.Option");
+        assertUnchanged("():haxe.__Int64");
 
         // hopefully this is never needed...
         var letterOverflow = '(${[for (i in 0...30) ":Int"].join(", ")}):Void';