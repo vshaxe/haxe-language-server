3f7a0a14306904f809d43d852d6657bf50766f1b
Jens Fischer <jensfischer95@gmail.com>
2017-05-27 19:14:01 +0200

    Add a simple test

---
+++ test/haxeLanguageServer/hxParser/LocalUsageResolverTest.hx
@@ -0,0 +1,57 @@
+package haxeLanguageServer.hxParser;
+
+import haxeLanguageServer.TextDocument;
+
+class LocalUsageResolverTest extends TestCaseBase {
+    function check(code:String) {
+        var ranges = findMarkedRanges(code, "%");
+        var declaration = ranges.shift();
+        var expectedUsages = ranges;
+
+        code = code.replace("%", "");
+
+        var resolver = new LocalUsageResolver(declaration);
+        resolver.walkFile(new TextDocument(new DocumentUri("file:///c:/"), "haxe", 0, code).parseTree, Root);
+        var actualUsages = resolver.usages;
+
+        function fail() {
+            throw 'Expected $expectedUsages but was $actualUsages';
+        }
+
+        if (expectedUsages.length != actualUsages.length) {
+            fail();
+        } else {
+            for (i in 0...expectedUsages.length) {
+                if (!expectedUsages[i].isEqual(actualUsages[i])) {
+                    fail();
+                }
+            }
+        }
+        currentTest.done = true;
+    }
+
+    function findMarkedRanges(code:String, marker:String):Array<Range> {
+        // not expecting multiple marked words in a single line..
+        var lineNumber = 0;
+        var ranges = [];
+        for (line in code.split("\n")) {
+            var startChar = line.indexOf(marker);
+            var endChar = line.lastIndexOf(marker);
+            if (startChar != -1 && endChar != -1) {
+                ranges.push({start: {line: lineNumber, character: startChar}, end: {line: lineNumber, character: endChar - 1}});
+            }
+            lineNumber++;
+        }
+        return ranges;
+    }
+
+    function testFindLocalVarUsages() {
+        check("
+class Foo {
+    function foo() {
+        var %bar%;
+        %bar%;
+    }
+}");
+    }
+}
