vshaxe/haxe-language-server

commit b8a1c213d9b06bab5680ea5c4c08e9f471c013ff

Improve capability handling
- remove supportsJsonRpc in favor of checking if the individual methods are supported
- always try calling the initialize method - if it errors, just set all capabilities to false

src/haxeLanguageServer/server/HaxeServer.hx
@@ -33,8 +33,7 @@ class HaxeServer {
     var nextRequestId:Int = 0;
 
     public var version(default,null):SemVer;
-    public var supportsJsonRpc(default,null):Bool = true; // TODO: don't lie
-    public var capabilities:HaxeCapabilities;
+    public var capabilities(default,null):HaxeCapabilities;
 
     public function new(context:Context) {
         this.context = context;
@@ -96,40 +95,25 @@ class HaxeServer {
             context.sendLogMessage(Log, reTrailingNewline.replace(buf.toString(), ""));
         });
         proc.stderr.on(ReadableEvent.Data, onData);
-
         proc.on(ChildProcessEvent.Exit, onExit);
-        if (context.displayArguments != null) {
-            function buildCompletionCache() {
-                if (!context.config.buildCompletionCache) {
-                    return;
-                }
-                stopProgressCallback = context.startProgress("Initializing Completion");
-                trace("Initializing completion cache...");
-                process(context.displayArguments.concat(["--no-output"]), null, true, null, Processed(function(_) {
-                    stopProgress();
-                    trace("Done.");
-                }, function(errorMessage) {
-                    stopProgress();
-                    trace("Failed - try fixing the error(s) and restarting the language server:\n\n" + errorMessage);
-                }));
-            }
 
-            if (supportsJsonRpc) {
-                stopProgressCallback = context.startProgress("Initializing Haxe/json-rpc protocol");
-                process(context.displayArguments.concat(["--display", context.haxeServer.createRequest(HaxeMethods.Initialize, {})]), null, true, null, Processed(function(result) {
-                    stopProgress();
-                    switch (result) {
-                        case DResult(capabilities): this.capabilities = Json.parse(capabilities).result.capabilities;
-                        case DCancelled:
-                    }
-                    buildCompletionCache();
-                }, function(errorMessage) {
-                    stopProgress();
-                }));
-            } else {
-                buildCompletionCache();
+        capabilities = {
+            definitionProvider: false,
+            hoverProvider: false
+        };
+
+        stopProgressCallback = context.startProgress("Initializing Haxe/JSON-RPC protocol");
+        process(["--display", createRequest(HaxeMethods.Initialize)], null, true, null, Processed(function(result) {
+            stopProgress();
+            switch (result) {
+                case DResult(capabilities):
+                    this.capabilities = Json.parse(capabilities).result.capabilities;
+                case DCancelled:
             }
-        }
+            buildCompletionCache();
+        }, function(errorMessage) {
+            stopProgress();
+        }));
 
         var displayPort = context.config.displayPort;
         if (socketListener == null && displayPort != null) {
@@ -142,6 +126,21 @@ class HaxeServer {
 
         if (callback != null)
             callback();
+    }
+
+    function buildCompletionCache() {
+        if (!context.config.buildCompletionCache || context.displayArguments == null) {
+            return;
+        }
+        stopProgressCallback = context.startProgress("Initializing Completion");
+        trace("Initializing completion cache...");
+        process(context.displayArguments.concat(["--no-output"]), null, true, null, Processed(function(_) {
+            stopProgress();
+            trace("Done.");
+        }, function(errorMessage) {
+            stopProgress();
+            trace("Failed - try fixing the error(s) and restarting the language server:\n\n" + errorMessage);
+        }));
     }
 
     function hasNonCancellableRequests():Bool {
@@ -341,7 +340,7 @@ class HaxeServer {
         }
     }
 
-     public function createRequest<P,R>(method:HaxeRequestMethod<P,R>, params:P):String {
+     public function createRequest<P,R>(method:HaxeRequestMethod<P,R>, ?params:P):String {
         // TODO: avoid duplicating jsonrpc.Protocol logic
         var id = nextRequestId++;
         var request:RequestMessage = {

src/haxeLanguageServer/features/GotoDefinitionFeature.hx
@@ -18,8 +18,9 @@ class GotoDefinitionFeature {
         var doc = context.documents.get(params.textDocument.uri);
         var bytePos = context.displayOffsetConverter.characterOffsetToByteOffset(doc.content, doc.offsetAt(params.position));
 
+        var useJsonRpc = context.haxeServer.capabilities.definitionProvider;
         var args =
-            if (context.haxeServer.supportsJsonRpc && context.haxeServer.capabilities.definitionProvider)
+            if (useJsonRpc)
                 [context.haxeServer.createRequest(HaxeMethods.GotoDefinition, {file: doc.fsPath, offset: bytePos})]
             else
                 ['${doc.fsPath}@$bytePos@position'];
@@ -29,7 +30,7 @@ class GotoDefinitionFeature {
                 case DCancelled:
                     resolve(null);
                 case DResult(data):
-                    if (context.haxeServer.supportsJsonRpc)
+                    if (useJsonRpc)
                         handleJson(data, resolve);
                     else
                         handleXml(data, doc, resolve, reject);

src/haxeLanguageServer/features/HoverFeature.hx
@@ -18,7 +18,8 @@ class HoverFeature {
     function onHover(params:TextDocumentPositionParams, token:CancellationToken, resolve:Hover->Void, reject:ResponseError<NoData>->Void) {
         var doc = context.documents.get(params.textDocument.uri);
         var bytePos = context.displayOffsetConverter.characterOffsetToByteOffset(doc.content, doc.offsetAt(params.position));
-        if (context.haxeServer.supportsJsonRpc && context.haxeServer.capabilities.hoverProvider) {
+        var useJsonRpc = context.haxeServer.capabilities.hoverProvider;
+        if (useJsonRpc) {
             var args = [context.haxeServer.createRequest(HaxeMethods.Hover, {file: doc.fsPath, offset: bytePos})];
             context.callDisplay(args, doc.content, token, function(result) {
                 var hover:HoverResult = switch (result) {

commit 3b4fba5cbd7d7bc5f7a4a1ebbce93552d62e7fcc

Fix completion cache not being built anymore if Haxe doesn't support JSON-RPC
regression from b8a1c21

src/haxeLanguageServer/server/HaxeServer.hx
@@ -104,15 +104,16 @@ class HaxeServer {
 
         stopProgressCallback = context.startProgress("Initializing Haxe/JSON-RPC protocol");
         process(["--display", createRequest(HaxeMethods.Initialize)], null, true, null, Processed(function(result) {
-            stopProgress();
             switch (result) {
                 case DResult(capabilities):
                     this.capabilities = Json.parse(capabilities).result.capabilities;
                 case DCancelled:
             }
+            stopProgress();
             buildCompletionCache();
         }, function(errorMessage) {
             stopProgress();
+            buildCompletionCache();
         }));
 
         var displayPort = context.config.displayPort;