eeaeeb32289f98a3b8ff90bd458a0a6f7ccf37da
Jens Fischer <jensfischer95@gmail.com>
2018-07-08 17:34:17 +0200

    Swap out haxe-formatter for tokentree-formatter

--- src/haxeLanguageServer/features/DocumentFormattingFeature.hx
+++ src/haxeLanguageServer/features/DocumentFormattingFeature.hx
@@ -1,6 +1,8 @@
 package haxeLanguageServer.features;
 
-import haxeFormatter.Formatter;
+import byte.ByteData;
+import tokentreeformatter.config.Config;
+import tokentreeformatter.Formatter;
 import jsonrpc.CancellationToken;
 import jsonrpc.ResponseError;
 import jsonrpc.Types.NoData;
@@ -15,15 +17,21 @@ class DocumentFormattingFeature {
 
     function onDocumentFormatting(params:DocumentFormattingParams, token:CancellationToken, resolve:Array<TextEdit>->Void, reject:ResponseError<NoData>->Void) {
         var doc = context.documents.get(params.textDocument.uri);
-        switch (Formatter.formatSource(doc.content, File, context.config.format)) {
-            case Success(s):
-                var fullRange = {
-                    start: {line: 0, character: 0},
-                    end: {line: doc.lineCount - 1, character: doc.lineAt(doc.lineCount - 1).length}
-                }
-                resolve([{range: fullRange, newText: s}]);
-            case Failure(reason):
-                reject(ResponseError.internalError(reason));
+        var formatter = new Formatter();
+        try {
+            var formattedFile = formatter.formatFile({
+                    name: doc.uri.toFsPath().toString(),
+                    content: ByteData.ofString(doc.content)
+                },
+                new Config()
+            );
+            var fullRange = {
+                start: {line: 0, character: 0},
+                end: {line: doc.lineCount - 1, character: doc.lineAt(doc.lineCount - 1).length}
+            }
+            resolve([{range: fullRange, newText: formattedFile}]);
+        } catch (e:Any) {
+           trace(e);
         }
     }
 }
