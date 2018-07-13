405df483c6fb21663bb0cede5220d3f421eec41d
Jens Fischer <jensfischer95@gmail.com>
2018-07-14 01:34:25 +0200

    Use tokentree-formatter for DocumentFormattingFeature

--- src/haxeLanguageServer/features/DocumentFormattingFeature.hx
+++ src/haxeLanguageServer/features/DocumentFormattingFeature.hx
@@ -1,5 +1,7 @@
 package haxeLanguageServer.features;
 
+import byte.ByteData;
+import tokentreeformatter.Formatter;
 import jsonrpc.CancellationToken;
 import jsonrpc.ResponseError;
 import jsonrpc.Types.NoData;
@@ -13,6 +15,25 @@ class DocumentFormattingFeature {
     }
 
     function onDocumentFormatting(params:DocumentFormattingParams, token:CancellationToken, resolve:Array<TextEdit>->Void, reject:ResponseError<NoData>->Void) {
-
+        var doc = context.documents.get(params.textDocument.uri);
+        var formatter = new Formatter();
+        try {
+            var formattedFile = formatter.formatFile({
+                    name: doc.uri.toFsPath().toString(),
+                    content: ByteData.ofString(doc.content)
+                }
+            );
+            if (formattedFile == null) {
+                resolve([]);
+                return;
+            }
+            var fullRange = {
+                start: {line: 0, character: 0},
+                end: {line: doc.lineCount - 1, character: doc.lineAt(doc.lineCount - 1).length}
+            }
+            resolve([{range: fullRange, newText: formattedFile}]);
+        } catch (e:Any) {
+            trace(e);
+        }
     }
 }
