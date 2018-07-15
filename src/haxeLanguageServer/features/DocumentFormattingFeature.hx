a7dce3d0790387c3f492275b7ce11493a4901a1b
Jens Fischer <jensfischer95@gmail.com>
2018-07-15 22:19:19 +0200

    Print exceptions that happen during formatting

--- src/haxeLanguageServer/features/DocumentFormattingFeature.hx
+++ src/haxeLanguageServer/features/DocumentFormattingFeature.hx
@@ -1,5 +1,6 @@
 package haxeLanguageServer.features;
 
+import haxe.Timer;
 import byte.ByteData;
 import tokentreeformatter.Formatter;
 import jsonrpc.CancellationToken;
@@ -15,23 +16,27 @@ class DocumentFormattingFeature {
     }
 
     function onDocumentFormatting(params:DocumentFormattingParams, token:CancellationToken, resolve:Array<TextEdit>->Void, reject:ResponseError<NoData>->Void) {
+        var stamp = Timer.stamp();
+
         var doc = context.documents.get(params.textDocument.uri);
         var formatter = new Formatter();
         try {
-            var formattedFile = formatter.formatFile({
+            var result = formatter.formatFile({
                     name: doc.uri.toFsPath().toString(),
                     content: ByteData.ofString(doc.content)
                 }
             );
-            if (formattedFile == null) {
-                reject(ResponseError.internalError("formatFile() returned null"));
-                return;
+            switch (result) {
+                case SUCCESS(formattedCode):
+                    var fullRange = {
+                        start: {line: 0, character: 0},
+                        end: {line: doc.lineCount - 1, character: doc.lineAt(doc.lineCount - 1).length}
+                    }
+                    trace("formatting took " + ((Timer.stamp() - stamp) * 1000) + "ms");
+                    resolve([{range: fullRange, newText: formattedCode}]);
+                case FAILURE(errorMessage):
+                    reject(ResponseError.internalError(errorMessage));
             }
-            var fullRange = {
-                start: {line: 0, character: 0},
-                end: {line: doc.lineCount - 1, character: doc.lineAt(doc.lineCount - 1).length}
-            }
-            resolve([{range: fullRange, newText: formattedFile}]);
         } catch (e:Any) {
             reject(ResponseError.internalError(e));
         }
