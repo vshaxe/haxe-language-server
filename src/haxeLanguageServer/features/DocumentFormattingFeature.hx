55c477f7c478facb9c47b39e68707cc3bb5cb641
Jens Fischer <jensfischer95@gmail.com>
2018-07-26 22:43:28 +0200

    Remove a redundant try-catch from DocumentFormattingFeature
    jsonrpc.Protocol already has a catch-all around the handler call.

--- src/haxeLanguageServer/features/DocumentFormattingFeature.hx
+++ src/haxeLanguageServer/features/DocumentFormattingFeature.hx
@@ -17,31 +17,27 @@ class DocumentFormattingFeature {
         var onResolve = context.startTimer(Methods.DocumentFormatting);
         var doc = context.documents.get(params.textDocument.uri);
         var formatter = new Formatter();
-        try {
-            var result = formatter.formatFile({
-                    name: doc.uri.toFsPath().toString(),
-                    content: doc.tokens.bytes
-                }, {
-                    tokens: doc.tokens.list,
-                    tokenTree: doc.tokens.tree
-                }
-            );
-            switch (result) {
-                case Success(formattedCode):
-                    var fullRange = {
-                        start: {line: 0, character: 0},
-                        end: {line: doc.lineCount - 1, character: doc.lineAt(doc.lineCount - 1).length}
-                    }
-                    var edits = [{range: fullRange, newText: formattedCode}];
-                    resolve(edits);
-                    onResolve(edits);
-                case Failure(errorMessage):
-                    reject(ResponseError.internalError(errorMessage));
-                case Disabled:
-                    reject(ResponseError.internalError("Formatting is disabled for this file"));
+        var result = formatter.formatFile({
+                name: doc.uri.toFsPath().toString(),
+                content: doc.tokens.bytes
+            }, {
+                tokens: doc.tokens.list,
+                tokenTree: doc.tokens.tree
             }
-        } catch (e:Any) {
-            reject(ResponseError.internalError(e));
+        );
+        switch (result) {
+            case Success(formattedCode):
+                var fullRange = {
+                    start: {line: 0, character: 0},
+                    end: {line: doc.lineCount - 1, character: doc.lineAt(doc.lineCount - 1).length}
+                }
+                var edits = [{range: fullRange, newText: formattedCode}];
+                resolve(edits);
+                onResolve(edits);
+            case Failure(errorMessage):
+                reject(ResponseError.internalError(errorMessage));
+            case Disabled:
+                reject(ResponseError.internalError("Formatting is disabled for this file"));
         }
     }
 }
