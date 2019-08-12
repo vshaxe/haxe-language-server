7ed4de1760c49e53c982deb088117c09d1a97ea7
Jens Fischer <jensfischer95@gmail.com>
2019-08-12 17:25:26 +0200

    Unit test compile fix

--- test/import.hx
+++ test/import.hx
@@ -1,6 +1,6 @@
 import languageServerProtocol.Types;
 import languageServerProtocol.protocol.Protocol;
-import haxeLanguageServer.helper.FsPath;
+import haxe.display.FsPath;
 import utest.Assert;
 import utest.Test;
 
@@ -9,3 +9,4 @@ using haxeLanguageServer.helper.RangeHelper;
 using haxeLanguageServer.helper.PositionHelper;
 using haxeLanguageServer.helper.ArrayHelper;
 using haxeLanguageServer.helper.DocumentUriHelper;
+using haxeLanguageServer.helper.FsPathHelper;
