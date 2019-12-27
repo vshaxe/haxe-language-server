6e0413015bce7ffdd272dca18b0864c6c3efb1f0
AlexHaxe <Alexander.Blum@gmail.com>
2019-12-27 22:51:01 +0100

    fixed newline at end of imports, fixes vshaxe/vshaxe#403
    fixed newline at end of imports in conditionals, fixes vshaxe/vshaxe#404

--- src/haxeLanguageServer/features/codeAction/OrganizeImportsFeature.hx
+++ src/haxeLanguageServer/features/codeAction/OrganizeImportsFeature.hx
@@ -15,8 +15,9 @@ class OrganizeImportsFeature {
 	];
 
 	public static function organizeImports(doc:TextDocument, context:Context, unusedRanges:Array<Range>):Array<TextEdit> {
-		if ((doc.tokens == null) || (doc.tokens.tree == null))
+		if ((doc.tokens == null) || (doc.tokens.tree == null)) {
 			return [];
+		}
 		try {
 			var packageName:Null<String> = null;
 			var imports:Array<TokenTree> = doc.tokens.tree.filterCallback(function(token:TokenTree, index:Int):FilterResult {
@@ -25,8 +26,9 @@ class OrganizeImportsFeature {
 						return FOUND_SKIP_SUBTREE;
 					case Kwd(KwdPackage):
 						var child = token.getFirstChild();
-						if (child == null)
+						if (child == null) {
 							return SKIP_SUBTREE;
+						}
 						switch (child.tok) {
 							case Kwd(_):
 								packageName = '$child';
@@ -41,10 +43,12 @@ class OrganizeImportsFeature {
 			});
 
 			var importGroups:Map<Int, ImportGroup> = new Map<Int, ImportGroup>();
+			var groupCount:Int = 0;
 			for (i in imports) {
 				var id:Int = -1;
-				if (i.parent != null)
+				if (i.parent != null) {
 					id = i.parent.index;
+				}
 
 				var group:Null<ImportGroup> = importGroups.get(id);
 				if (group == null) {
@@ -55,6 +59,7 @@ class OrganizeImportsFeature {
 						usings: []
 					}
 					importGroups.set(id, group);
+					groupCount++;
 				}
 
 				var range:Range = doc.rangeAt2(doc.tokens.getTreePos(i));
@@ -65,11 +70,11 @@ class OrganizeImportsFeature {
 						break;
 					}
 				}
-				if (isUnused)
+				if (isUnused) {
 					continue;
+				}
 
 				var type:ImportType = determineImportType(i, packageName);
-
 				switch (i.tok) {
 					case Kwd(KwdImport):
 						group.imports.push({
@@ -93,8 +98,9 @@ class OrganizeImportsFeature {
 
 	static function determineImportType(token:TokenTree, packageName:Null<String>):ImportType {
 		var child:Null<TokenTree> = token.getFirstChild();
-		if (child == null)
+		if (child == null) {
 			return Project;
+		}
 		var topLevelPack:String;
 		switch (child.tok) {
 			case Kwd(_):
@@ -104,49 +110,53 @@ class OrganizeImportsFeature {
 			default:
 				return Project;
 		}
-		if (stdLibPackages.contains(topLevelPack))
+		if (stdLibPackages.contains(topLevelPack)) {
 			return StdLib;
-		if (packageName == null)
+		}
+		if (packageName == null) {
 			return Project;
-		if (topLevelPack == packageName)
+		}
+		if (topLevelPack == packageName) {
 			return Project;
+		}
 		return Library;
 	}
 
 	static function organizeImportGroups(doc:TextDocument, context:Context, importGroups:Map<Int, ImportGroup>):Array<TextEdit> {
 		var edits:Array<TextEdit> = [];
-
-		for (group in importGroups)
+		for (group in importGroups) {
 			edits = edits.concat(organizeImportGroup(doc, context, group));
-
+		}
 		return edits;
 	}
 
 	static function organizeImportGroup(doc:TextDocument, context:Context, importGroup:ImportGroup):Array<TextEdit> {
 		var sortFunc:Null<ImportSortFunction> = determineSortFunction(context);
-		if (sortFunc == null)
+		if (sortFunc == null) {
 			return [];
+		}
 
 		ArraySort.sort(importGroup.imports, sortFunc);
 		var newImports:String = importGroup.imports.map(i -> i.text).join("\n");
 
 		ArraySort.sort(importGroup.usings, sortFunc);
 		var newUsings:String = importGroup.usings.map(i -> i.text).join("\n");
+		var delim:String = (importGroup.imports.length > 0 && importGroup.usings.length > 0) ? "\n" : "";
 
-		var newText:String = FormatterHelper.formatText(doc, context, newImports + "\n" + newUsings, TokenTreeEntryPoint.TYPE_LEVEL);
-
+		var newText:String = FormatterHelper.formatText(doc, context, newImports + delim + newUsings, TokenTreeEntryPoint.TYPE_LEVEL);
 		var edits:Array<TextEdit> = [];
 
 		// remove all existing imports/usings from group
-		for (i in importGroup.imports)
+		for (i in importGroup.imports) {
 			edits.push(makeImportEdit(doc, i.token));
-
-		for (i in importGroup.usings)
+		}
+		for (i in importGroup.usings) {
 			edits.push(makeImportEdit(doc, i.token));
+		}
 
 		// insert sorted imports/usings at startOffset
 		var importInsertPos:Position = doc.positionAt(importGroup.startOffset);
-		edits.push(WorkspaceEditHelper.insertText(importInsertPos, newText + "\n"));
+		edits.push(WorkspaceEditHelper.insertText(importInsertPos, newText));
 
 		return edits;
 	}
@@ -177,37 +187,45 @@ class OrganizeImportsFeature {
 			}
 		};
 		var lineAfter:String = doc.getText(nextLineRange).trim();
-		if (lineAfter.length <= 0)
-			range.end.line++;
-
+		if (lineAfter.length <= 0) {
+			// range.end.line++;
+		}
 		return WorkspaceEditHelper.removeText(range);
 	}
 
 	static function sortImportsAllAlpha(a:ImportInfo, b:ImportInfo):Int {
-		if (a.text < b.text)
+		if (a.text < b.text) {
 			return -1;
-		if (a.text > b.text)
+		}
+		if (a.text > b.text) {
 			return 1;
+		}
 		return 0;
 	}
 
 	static function sortImportsStdlibThenLibsThenProject(a:ImportInfo, b:ImportInfo):Int {
-		if (a.type < b.type)
+		if (a.type < b.type) {
 			return -1;
-		if (a.type > b.type)
+		}
+		if (a.type > b.type) {
 			return 1;
-		if (a.text < b.text)
+		}
+		if (a.text < b.text) {
 			return -1;
-		if (a.text > b.text)
+		}
+		if (a.text > b.text) {
 			return 1;
+		}
 		return 0;
 	}
 
 	static function sortImportsNonProjectThenProject(a:ImportInfo, b:ImportInfo):Int {
-		if (a.type == StdLib)
+		if (a.type == StdLib) {
 			a.type = Library;
-		if (b.type == StdLib)
+		}
+		if (b.type == StdLib) {
 			b.type = Library;
+		}
 		return sortImportsStdlibThenLibsThenProject(a, b);
 	}
 
