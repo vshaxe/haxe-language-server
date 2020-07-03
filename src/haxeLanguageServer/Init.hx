c33659f0a80f40b3e0dd9c2c3c4539637397728d
Jens Fischer <jensfischer95@gmail.com>
2020-07-03 22:37:18 +0200

    Replace uglify-es with terser, closes vshaxe/vshaxe#448

--- src/haxeLanguageServer/Init.hx
+++ src/haxeLanguageServer/Init.hx
@@ -1,7 +1,10 @@
 package haxeLanguageServer;
 
+import haxe.macro.Compiler;
+
 function run() {
 	#if debug
-	haxe.macro.Compiler.define("uglifyjs_disabled");
+	Compiler.define("uglifyjs_disabled");
 	#end
+	Compiler.define("uglifyjs_bin=" + (if (Sys.systemName() == "Windows") "node_modules\\.bin\\terser.cmd" else "./node_modules/.bin/terser"));
 }
