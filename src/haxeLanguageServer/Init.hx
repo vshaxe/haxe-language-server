ebadbbac8ccf9313a3f1d2fdfd2ac54132491c21
Sebastian Thomschke <sebthom@users.noreply.github.com>
2022-03-21 16:14:43 +0100

    Fix "'uglifyjs' is not recognized" on Windows
    Without this fix I am getting the following error when trying to build on Windows:
    ```
    > npx lix run vshaxe-build -t language-server
    'uglifyjs' is not recognized as an internal or external command,
    operable program or batch file.
    ```

--- src/haxeLanguageServer/Init.hx
+++ src/haxeLanguageServer/Init.hx
@@ -6,5 +6,5 @@ function run() {
 	#if debug
 	Compiler.define("uglifyjs_disabled");
 	#end
-	Compiler.define("uglifyjs_bin=" + (if (Sys.systemName() == "Windows") "node_modules\\.bin\\terser.cmd" else "./node_modules/.bin/terser"));
+	Compiler.define("uglifyjs_bin", (if (Sys.systemName() == "Windows") "node_modules\\.bin\\terser.cmd" else "./node_modules/.bin/terser"));
 }
